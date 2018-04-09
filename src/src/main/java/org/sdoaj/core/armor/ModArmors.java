9497cd2f8507f0f6c9f6ec3ef974b17a94292523
AdityaGupta1 <adityag@gmail.com>
2018-04-09 15:53:48 -0700

    added ender armor, fixed nether armor item colors

--- src/src/main/java/org/sdoaj/core/armor/ModArmors.java
+++ src/src/main/java/org/sdoaj/core/armor/ModArmors.java
@@ -16,4 +16,13 @@ public class ModArmors {
     public static final ItemArmor nether_chestplate = new ItemArmor("nether_chestplate", NETHER_ARMOR_MATERIAL, EntityEquipmentSlot.CHEST);
     public static final ItemArmor nether_leggings = new ItemArmor("nether_leggings", NETHER_ARMOR_MATERIAL, EntityEquipmentSlot.LEGS);
     public static final ItemArmor nether_boots = new ItemArmor("nether_boots", NETHER_ARMOR_MATERIAL, EntityEquipmentSlot.FEET);
+
+    public static final ItemArmor.ArmorMaterial ENDER_ARMOR_MATERIAL =
+            EnumHelper.addArmorMaterial("ENDER", Main.MODID + ":ender", 64,
+                    new int[]{5, 12, 8, 5}, 36, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 4F)
+                    .setRepairItem(new ItemStack(ModItems.ender_core));
+    public static final ItemArmor ender_helmet = new ItemArmor("ender_helmet", ENDER_ARMOR_MATERIAL, EntityEquipmentSlot.HEAD);
+    public static final ItemArmor ender_chestplate = new ItemArmor("ender_chestplate", ENDER_ARMOR_MATERIAL, EntityEquipmentSlot.CHEST);
+    public static final ItemArmor ender_leggings = new ItemArmor("ender_leggings", ENDER_ARMOR_MATERIAL, EntityEquipmentSlot.LEGS);
+    public static final ItemArmor ender_boots = new ItemArmor("ender_boots", ENDER_ARMOR_MATERIAL, EntityEquipmentSlot.FEET);
 }
